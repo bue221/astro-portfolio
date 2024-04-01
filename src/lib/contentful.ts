d0ed83f027f210e7aa007531d776b2305dd5fa7f
bue221 <camilo.plaza@spot2.mx>
2024-04-01 09:54:51 -0500

    Update Contentful access token and host configuration

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -2,8 +2,8 @@ import contentful from 'contentful'
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: false
+  accessToken: import.meta.env.DEV
     ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
     : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-  host: false ? 'preview.contentful.com' : 'cdn.contentful.com',
+  host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com',
 })
