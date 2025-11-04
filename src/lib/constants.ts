01508c6625858dd723dad972d3badeaa1bbb314e
Andres Camilo Plaza Jimenez <camiloplaza3@gmail.com>
2025-11-04 03:16:19 -0500

    Revert "feat: Implement i18n and update work history"

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -15,6 +15,38 @@ export const LINKS = {
   discord: 'https://discordapp.com/users/163300027618295808',
 }
 
+// Global
+export const SITE: Site = {
+  TITLE: 'Astro Sphere',
+  DESCRIPTION:
+    'Welcome to Astro Sphere, a portfolio and blog for designers and developers.',
+  AUTHOR: 'Mark Horn',
+}
+
+// Work Page
+export const WORK: Page = {
+  TITLE: 'Work',
+  DESCRIPTION: 'Places I have worked.',
+}
+
+// Blog Page
+export const BLOG: Page = {
+  TITLE: 'Blog',
+  DESCRIPTION: 'Writing on topics I am passionate about.',
+}
+
+// Projects Page
+export const PROJECTS: Page = {
+  TITLE: 'Projects',
+  DESCRIPTION: 'Recent projects I have worked on.',
+}
+
+// Search Page
+export const SEARCH: Page = {
+  TITLE: 'Search',
+  DESCRIPTION: 'Search all posts and projects by keyword.',
+}
+
 // Study Page
 export const STUDIES = [
   {
@@ -51,83 +83,78 @@ export const STUDIES = [
 
 export const EXPERIENCE = [
   {
-    company: 'experience.mercadoLibre.company',
-    location: 'Buenos Aires, Argentina',
-    position: 'experience.mercadoLibre.position',
-    start: '2025',
-    link: 'https://mercadolibre.com/',
-    end: 'Current',
-    tasks: [
-      'experience.mercadoLibre.tasks.0',
-      'experience.mercadoLibre.tasks.1',
-      'experience.mercadoLibre.tasks.2',
-    ],
-  },
-  {
-    company: 'experience.straico.company',
+    company: 'Straico',
     location: 'Bogotá D C, Colombia',
-    position: 'experience.straico.position',
+    position: 'Software Engineer',
     start: '2021',
     link: 'https://straico.com/',
-    end: '2025',
-    tasks: ['experience.straico.tasks.0', 'experience.straico.tasks.1'],
-  },
-  {
-    company: 'experience.spot2.company',
-    location: 'Mexico City, Mexico',
-    position: 'experience.spot2.position',
-    link: 'https://spot2.mx/',
-    start: '2021',
-    end: '2025',
+    end: 'Current',
     tasks: [
-      'experience.spot2.tasks.0',
-      'experience.spot2.tasks.1',
-      'experience.spot2.tasks.2',
-      'experience.spot2.tasks.3',
+      'Integration IA services with react and tailwind css',
+      'Development and build of DB with mongoDB',
     ],
   },
   {
-    company: 'experience.imaginamos.company',
-    link: 'https://imaginamos.com/',
-    location: 'Bogotá D C, Colombia',
-    position: 'experience.imaginamos.position',
+    company: 'Spot2',
+    location: 'Mexico City, Mexico',
+    position: 'Software Engineer',
+    link: 'https://spot2.mx/',
     start: '2021',
-    end: '2021',
-    tasks: ['experience.imaginamos.tasks.0', 'experience.imaginamos.tasks.1'],
+    end: 'Current',
+    tasks: [
+      'Development of the Spot2 platform with the use of React, Redux, and Material UI',
+      'Development map with the use of Google Maps API',
+      'Development internal platform with the use of React, Redux, and Material UI',
+      'Testing and debugging',
+    ],
   },
   {
-    company: 'experience.inetum.company',
+    company: 'Imaginamos',
+    link: 'https://imaginamos.com/',
     location: 'Bogotá D C, Colombia',
-    position: 'experience.inetum.position',
+    position: 'Frontend developer',
+    start: '2021',
+    end: '2021',
+    tasks: [
+      'Development of the Imaginamos platform with the use of React, Redux, and Material UI',
+      'Work in ETB project with the use of React, Redux, and Material UI',
+    ],
+  },
+  {
+    company: 'INETUM',
+    location: 'Bogotá D C, Colombia',
+    position: 'Frontend developer',
     start: '2021',
     link: 'https://www.inetum.com/es',
     end: '2021',
     tasks: [
-      'experience.inetum.tasks.0',
-      'experience.inetum.tasks.1',
-      'experience.inetum.tasks.2',
-      'experience.inetum.tasks.3',
+      'Support in the QA area and bug review',
+      'Use of SCRUM methodology',
+      'Claro projects with the use of frameworks and libraries such as react and angular',
+      'Use of redux toolkit as aproposal which allowed a shorter development time when using this tool',
     ],
   },
   {
-    company: 'experience.sig.company',
+    company: 'S I G',
     location: 'Bogotá D C, Colombia',
-    position: 'experience.sig.position',
+    position: 'Software developer',
     start: '2021',
     end: '2021',
-    tasks: ['experience.sig.tasks.0'],
+    tasks: [
+      'Development of a dashboard with react admin and chartjs for managing and entering information',
+    ],
   },
   {
-    company: 'experience.bookii.company',
+    company: 'BOOKII',
     location: 'Bogotá D C, Colombia',
-    position: 'experience.bookii.position',
+    position: 'Software developer',
     start: '2019',
     end: '2021',
     tasks: [
-      'experience.bookii.tasks.0',
-      'experience.bookii.tasks.1',
-      'experience.bookii.tasks.2',
-      'experience.bookii.tasks.3',
+      'Productive collaborationwiththebackendteamforthecreationofthe conversationalclubs',
+      'Effective coding of conversational clubs following design guide lines and using the Redux statemanager',
+      'Creation of the Bookii page in Spanish and change of texts in cms',
+      'Generation of static posts using Gatsby and the Contentful cms for the Bookii blog',
     ],
   },
 ]
